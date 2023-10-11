d67130d3412c17d46e72030bcefa5e32f6edf7df
Furkan Cingöz <efcingoz@outlook.com>
2023-10-11 17:07:02 +0300

    switch is on durumu kontrol etme

--- widget kullanimi/ViewController.swift
+++ widget kullanimi/ViewController.swift
@@ -9,29 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var labelText: UILabel!
-    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textField: UITextField!
-    
+    @IBOutlet weak var switchOutlet: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
     @IBAction func yapButton(_ sender: Any) {
         if let alinanVeri = textField.text {
             labelText.text = alinanVeri
         }
-        
+    }
+    @IBAction func switchButton(_ sender: UISwitch) {
+        if sender.isOn {
+            print("on")
+        } else {
+            print("off")
+        }
     }
     
-    @IBAction func smileButton(_ sender: Any) {
-        imageView.image = UIImage(named: "smile")
+        @IBAction func showButton(_ sender: Any) {
+            print("switch son hali:\(switchOutlet.isOn)")
+            
+        }
+        @IBAction func smileButton(_ sender: Any) {
+            imageView.image = UIImage(named: "smile")
+        }
+        @IBAction func sadButton(_ sender: Any) {
+            imageView.image = UIImage(named: "sad")
+        }
     }
     
-    @IBAction func sadButton(_ sender: Any) {
-        imageView.image = UIImage(named: "sad")
-    }
-    
-}
 
