98e4df4cff3620297a1efcb4e3953cf9c66ac3b1
Furkan Cingöz <efcingoz@outlook.com>
2023-10-11 16:02:01 +0300

    Initial Commit

---
+++ widget kullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  widget kullanimi
+//
+//  Created by Furkan Cingöz on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
