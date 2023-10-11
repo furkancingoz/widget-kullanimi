4b86778625e9eea84c6d6e7b8dcde30bf5d58ce3
Furkan Cingöz <efcingoz@outlook.com>
2023-10-11 16:57:27 +0300

    xdddd resim değiştirebiliyorum

--- widget kullanimi/ViewController.swift
+++ widget kullanimi/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelText: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func yapButton(_ sender: Any) {
+        if let alinanVeri = textField.text {
+            labelText.text = alinanVeri
+        }
+        
+    }
+    
+    @IBAction func smileButton(_ sender: Any) {
+        imageView.image = UIImage(named: "smile")
+    }
+    
+    @IBAction func sadButton(_ sender: Any) {
+        imageView.image = UIImage(named: "sad")
+    }
+    
 }
 
